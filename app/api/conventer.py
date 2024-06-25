6fcadba032a45b552d246627226d4015fd82ef93
MaximPyanin <maximpyanin@icloud.com>
2024-06-25 21:01:57 +0300

    feat(ruff.toml): add lint and format configurations to the project for consistent code style and quality control

--- app/api/conventer.py
+++ app/api/conventer.py
@@ -1,6 +1,9 @@
 import os.path
 
+from sqlalchemy import select
 from fastapi import APIRouter
+
+from sqlalchemy.ext.asyncio import create_async_engine
 from fastapi.responses import FileResponse
 from fastapi import FastAPI, Path, Query, UploadFile, File, HTTPException
 from app.utils.convert_txt import ConvertTXT
@@ -8,13 +11,18 @@ from app.utils.convert_txt import ConvertTXT
 
 class Converter:
     def __init__(self):
-        self.router = APIRouter(prefix="/api/v1", tags=["convertor"])
+        self.router = APIRouter(
+            prefix="/api/v1",
+            tags=["convertor"],
+        )
 
     def get_router(self) -> APIRouter:
         self.router.post("/pdf")(self.convert)
+        self.router.get("/health")(self.check_connection)
         return self.router
 
     async def convert(self, file: UploadFile = File()) -> FileResponse:
+        # ,check: Check.check_ip_browser = Depends()
         if file.content_type != "text/plain":
             raise HTTPException(
                 status_code=415,
@@ -28,3 +36,13 @@ class Converter:
         return FileResponse(
             f"..utils.{name}.pdf", media_type="application/pdf", filename=f"{name}.pdf"
         )
+
+    async def check_connection(self) -> dict:
+        try:
+            async with create_async_engine(
+                url="postgresql+asyncpg://postgres:qwrqer325345@localhost:5432",
+            ).connect() as connection:
+                await connection.execute(select(1))
+            return {"status": "ok"}
+        except Exception:
+            raise HTTPException(status_code=500, detail="Database connection error")
